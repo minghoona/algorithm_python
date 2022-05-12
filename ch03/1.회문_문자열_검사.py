3b697a99d37c08bb082e141f0ce4f53da9c1ce03
minghoona <trkmh0718@gmail.com>
2022-05-12 18:40:53 +0900

    챕터별 폴더 나누기. ch03. 3.카드_역배치까지 풀이

---
+++ ch03/1.회문_문자열_검사.py
@@ -0,0 +1,19 @@
+n=int(input())
+for i in range(n):
+    s=input()
+    s=s.upper()
+    size=len(s)
+    #sol 1
+    '''
+    for j in range(size//2):
+        if s[j]!=s[-1-j]:
+            print("#%d NO" %(i+1))
+            break
+    else:
+        print("#%d YES" %(i+1))
+    '''
+    #sol 2
+    if s==s[::-1]:
+        print("#%d YES" % (i+1))
+    else:
+        print("#%d NO" % (i+1))
---
+++ ch03/3.카드_역배치.py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("input.txt", "rt")
+a=list(range(21))
+for _ in range(10):
+    s, e = map(int, input().split())
+    for i in range((e-s+1)//2):
+        a[s+i], a[e-i]=a[e-i], a[s+i]
+print(a)
