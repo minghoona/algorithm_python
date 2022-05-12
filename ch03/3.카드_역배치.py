1e05bc48297290a45cadc89db2f085d706f8be8e
minghoona <trkmh0718@gmail.com>
2022-05-12 18:43:59 +0900

    코드 수정

--- ch03/3.카드_역배치.py
+++ ch03/3.카드_역배치.py
@@ -5,4 +5,7 @@ for _ in range(10):
     s, e = map(int, input().split())
     for i in range((e-s+1)//2):
         a[s+i], a[e-i]=a[e-i], a[s+i]
-print(a)
+
+a.pop(0)
+for x in a:
+    print(x, end=' ')
